8b95412cde2bb0db7be1e6d3d3f7e9d5049e0e50
Justin Collier <justincollier256@gmail.com>
2025-01-14 16:53:43 -0700

    user can't spam submit id anymore and now there is loading visuals

--- src/app/enter-steam-id/enter-steam-id.component.ts
+++ src/app/enter-steam-id/enter-steam-id.component.ts
@@ -2,19 +2,22 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   standalone: true,
   selector: 'app-enter-steam-id',
   templateUrl: './enter-steam-id.component.html',
   styleUrls: ['./enter-steam-id.component.css'],
-  imports: [FormsModule]
+  imports: [CommonModule, FormsModule]
 })
 export class EnterSteamIdComponent {
   steamId: string = '';
+  loading = false;
 
-  constructor(private router: Router, private http: HttpClient) {}
-
+  constructor(private http: HttpClient, private router: Router) {}
+  
   onSubmit() {
     if (this.steamId.trim()) {
       console.log('Steam ID submitted:', this.steamId);
@@ -22,6 +25,8 @@ export class EnterSteamIdComponent {
       const url = 'http://localhost:8000/submit-steam-id'; 
       const body = { steamId: this.steamId };
       const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+      this.loading = true;
 
       this.http.post(url, body, { headers }).subscribe({
         next: (response) => {
@@ -31,6 +36,11 @@ export class EnterSteamIdComponent {
         error: (error) => {
           console.error('Error submitting Steam ID:', error);
           alert(`Error: ${error.error.detail}`);
+          this.loading = false;
+          this.steamId = '';
+        },
+        complete: () => {
+          this.loading = false;
         }
       });
     } else {
