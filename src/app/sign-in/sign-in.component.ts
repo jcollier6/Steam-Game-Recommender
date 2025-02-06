8f3495b56ef62abddc362d2f74a618ac75b10b25
Justin Collier <justincollier256@gmail.com>
2025-02-05 18:20:11 -0700

    new sign in page now connects to old recommendation page

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,12 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, ChangeDetectorRef } from '@angular/core';
+import { ReactiveFormsModule, FormControl, Validators, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -20,4 +22,44 @@ export class SignInComponent {
   get isInvalid() {
     return this.idInput.invalid && this.idInput.touched;
   }
+
+  steamId: string = '';
+    loading = false;
+  
+    constructor(
+      private http: HttpClient, 
+      private router: Router,
+      private cdr: ChangeDetectorRef) {}
+    
+    onSubmit() {
+      if (this.steamId.trim()) {
+        console.log('Steam ID submitted:', this.steamId);
+  
+        const url = 'http://localhost:8000/submit-steam-id'; 
+        const body = { steamId: this.steamId };
+        const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  
+        this.loading = true;
+  
+        this.http.post(url, body, { headers }).subscribe({
+          next: (response) => {
+            console.log('Response from server:', response);
+            this.router.navigate(['/home-page']);
+          },
+          error: (error) => {
+            console.error('Error submitting Steam ID:', error);
+            alert(`Error: ${error.error.detail}`);
+            this.loading = false;
+            this.steamId = '';
+            this.cdr.detectChanges();
+          },
+          complete: () => {
+            this.loading = false;
+            this.cdr.detectChanges();
+          }
+        });
+      } else {
+        console.error('Steam ID is required.');
+      }
+    }
 }
