65219af5b8848e1ea5ad590cd497a6028f122b88
Justin Collier <justincollier256@gmail.com>
2025-02-05 17:42:43 -0700

    added animations package

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
+    provideRouter(routes),
+    BrowserAnimationsModule,
+    provideAnimationsAsync()
   ]
 }).catch(err => console.error(err));
